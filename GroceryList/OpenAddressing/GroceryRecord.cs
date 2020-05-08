c7874e7c9393aeb9ce3a0d81fc5d5f508564c66d
Serena Bailey <47674941+BaileySerena@users.noreply.github.com>
2020-05-08 14:06:46 -0500

    Add files via upload
    Fixed

---
+++ GroceryList/OpenAddressing/GroceryRecord.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenAddressing
+{
+    class GroceryRecord
+    {
+        private int groceryID;
+        private String groceryName;
+
+        public GroceryRecord(int i, String name)
+        {
+            groceryID = i;
+            groceryName = name;
+        }
+
+        public int getgroceryId()
+        {
+            return groceryID;
+        }
+
+        public void setgroceryId(int i)
+        {
+            groceryID = i;
+        }
+        
+        public String toString()
+        {
+            return groceryID + " " + groceryName + " ";
+        }
+    }
+}
